0cf9989e52669ea46dc8a5c9feb4a25b03de584d
Pavin Pichienjaros <pavin.ss2@gmail.com>
2020-11-19 03:25:52 +0000

    Automatically backed up by Learn

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -78,3 +78,47 @@ describe('flow-control', () => {
     })
   })
 })
+
+function basicTeenager(age) {
+if (age >= 13 && age <= 19){
+return "You are a teenager!"
+}
+}
+
+function teenager(age) {
+if (age >= 13 && age <= 19){
+return "You are a teenager!"
+}
+else {
+return "You are not a teenager"
+
+}
+
+}
+
+function ageChecker(age) {
+if (age <= 12){
+return "You are a kid"
+}
+else if (age >= 13 && age <= 19) {
+return "You are a teenager!"
+}
+else{
+return "You are a grownup"}
+}
+
+function ternaryTeenager(age)
+{
+(age >= 1 && age <= 19) ? return "You are a teenager": return "You are not a teenager"
+
+}
+
+function switchAge(age) {
+switch(age){
+case (age >= 13 && age <=19):
+console.log("You are a teenager");
+break;
+default:
+console.log("You have an age");
+}
+}
