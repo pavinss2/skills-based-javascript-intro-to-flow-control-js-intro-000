246896fbec0e534ada2aed2b1ca3fdcad2f7d50b
Pavin Pichienjaros <pavin.ss2@gmail.com>
2020-11-19 03:29:08 +0000

    Automatically backed up by Learn

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -1,30 +1,7 @@
-const expect = require('expect')
-const fs = require('fs')
-const jsdom = require('jsdom')
-const path = require('path')
-
-describe('flow-control', () => {
-
-  before(done => {
-    const src = path.resolve(__dirname, '..', 'flow-control.js')
-
-    jsdom.env('<div></div>', [src], (err, window) => {
-      if (err) {
-        return done(err)
-      }
-
-      Object.keys(window).forEach(key => {
-        global[key] = window[key]
-      })
-
-      return done()
-    })
-  })
 
 function basicTeenager(age) {
 if (age >= 13 && age <= 19){
-return "You are a teenager!"
-}
+return "You are a teenager!"}
 }
 
 function teenager(age) {
@@ -32,10 +9,7 @@ if (age >= 13 && age <= 19){
 return "You are a teenager!"
 }
 else {
-return "You are not a teenager"
-
-}
-
+return "You are not a teenager"}
 }
 
 function ageChecker(age) {
